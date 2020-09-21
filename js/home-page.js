56299b63e2f8619ce594d08b6b1e795dfef7534b
Mohit Patel <3443mohit@gmail.com>
2020-09-22 01:43:30 +0530

    initial commit

---
+++ js/home-page.js
@@ -0,0 +1,34 @@
+$(function() {
+	// Blog JS
+  $(".sensee-media-slider").slick({
+    arrows: true,
+    dots: false,
+    infinite: true,
+    speed: 1400,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 992,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 575,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  });
+});
