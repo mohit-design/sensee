7893cef06979ebc6a008be96aacd8f16b8b9fa9f
Mohit Patel <3443mohit@gmail.com>
2020-09-23 23:52:27 +0530

    changes in menu

--- js/main.js
+++ js/main.js
@@ -1,3 +1,17 @@
 $(function() {
-	
+	// Mobile Menu JS
+  $(".burger-menu").click(function() {
+    $("body").addClass("active");
+    $(".bg-blur").addClass("active");
+    $(".fixed-navbar-menu-mobile").addClass("active");
+    $(".menu-listing-close-mobile").addClass("active");
+    $(".menu-listing-mobile").addClass("active");
+  });
+  $(".menu-listing-close-mobile").click(function() {
+    $("body").removeClass("active");
+    $(".bg-blur").removeClass("active");
+    $(".fixed-navbar-menu-mobile").removeClass("active");
+    $(".menu-listing-close-mobile").removeClass("active");
+    $(".menu-listing-mobile").removeClass("active");
+  });
 });
